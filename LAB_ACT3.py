5121e5c9d94e0702b9ecead6c7f096baba0bab99
AnterJoana <anterjoana511@gmail.com>
2025-03-11 22:03:00 +0800

    Add files via upload

---
+++ LAB_ACT3.py
@@ -0,0 +1,6 @@
+while True:
+  check_number = int(input("Cite a number. "))
+  if check_number % 2 ==0:
+      print(f"{check_number} is an Even number")
+  else:
+      print(f"{check_number} is an Odd number")
